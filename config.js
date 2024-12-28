b59bff0aa07f87dc27a7131691c055b824eb9826
Pasidubot <114871081+Pasidubot@users.noreply.github.com>
2024-12-28 17:43:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "S6gRlaoS#oZqTtbeaomGUr659LY7aeSX_lmGYyXS9wRWVO9rtrRs", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
